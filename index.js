874efe3e7c7d197cde9e101eb45b278f8d559eed
Paul Jomy <49817584+pauljomy@users.noreply.github.com>
2026-01-08 01:27:12 +0530

    homepapge rendering completed

---
+++ index.js
@@ -0,0 +1,33 @@
+import { menuArray } from "./data.js";
+
+const itemContainer = document.getElementById("item-container");
+
+console.log("menuArray:", menuArray);
+
+function render(items) {
+  let feedHTML = "";
+  menuArray.forEach((item) => {
+    feedHTML += ` <div class="item">
+        <div class="item-img">
+            ${item.emoji}
+        </div>
+        <div class="item-detail">
+            <div class="item-title">
+                <h2>${item.name}</h2>
+            </div>
+            <div class="item-ingredients">
+                <p>${item.ingredients.join(", ")}</p>
+            </div>
+            <div class="item-price">
+                <p>$${item.price}</p>
+            </div>
+        </div>
+        <div class="add-item-btn">
+            <button><img src="img/plus.svg" alt="plus-icon" /></button>
+        </div>
+    </div>`;
+  });
+  itemContainer.innerHTML = feedHTML;
+}
+
+render(menuArray);
